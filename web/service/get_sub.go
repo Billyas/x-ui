3b4790a79625da1baad441bcd04b5eac3aa9cad3
Billyas <62041096+Billyas@users.noreply.github.com>
2024-12-22 20:53:19 +0800

    Update get_sub.go
    添加请求头v2rayN,

--- web/service/get_sub.go
+++ web/service/get_sub.go
@@ -28,35 +28,48 @@ func (s *GetSubService) GetLatestUrlSub() (string, error) {
 	}
 
 	// 1. 对所有订阅进行HTTP请求并解码base64响应
-	subs, err := s.subService.GetSubsBySubType(model.SubURL)
-
-	if err == nil && len(subs) > 0 {
-		for _, sub := range subs {
-			response, err := http.Get(sub.Url)
-			if err != nil {
-				logger.Errorf(err.Error())
-				continue
-			}
-
-			defer response.Body.Close()
-
-			body, err := io.ReadAll(response.Body)
-			if err != nil {
-				logger.Errorf(err.Error())
-				continue
-			}
-
-			subStr, err := base64.StdEncoding.DecodeString(string(body))
-			if err != nil {
-				logger.Errorf(err.Error())
-				continue
-			}
-
-			stringBuilder.WriteString(string(subStr))
-		}
-	} else if err != nil {
-		logger.Errorf("步骤1：" + err.Error())
-	}
+	    subs, err := s.subService.GetSubsBySubType(model.SubURL)
+	    if err == nil && len(subs) > 0 {
+	        for _, sub := range subs {
+	            client := &http.Client{
+	                Transport: &http.Transport{
+	                    TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	                },
+	            }
+	
+	            req, err := http.NewRequest("GET", sub.Url, nil)
+	            if err != nil {
+	                logger.Errorf(err.Error())
+	                continue
+	            }
+	
+	            req.Header.Set("User-Agent", "V2rayN/5.0.1 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
+	
+	            response, err := client.Do(req)
+	            if err != nil {
+	                logger.Errorf(err.Error())
+	                continue
+	            }
+	
+	            defer response.Body.Close()
+	
+	            body, err := io.ReadAll(response.Body)
+	            if err != nil {
+	                logger.Errorf(err.Error())
+	                continue
+	            }
+	
+	            subStr, err := base64.StdEncoding.DecodeString(string(body))
+	            if err != nil {
+	                logger.Errorf(err.Error())
+	                continue
+	            }
+	
+	            stringBuilder.WriteString(string(subStr))
+	        }
+	    } else if err != nil {
+	        logger.Errorf("步骤1：" + err.Error())
+	    }
 
 	// 2. 获取自定义节点并将其添加到订阅内容中
 	nodes, err := s.subService.GetSubsBySubType(model.OwnNode)
